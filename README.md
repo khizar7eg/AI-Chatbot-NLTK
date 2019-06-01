411baa9f82b1f9d1e9a0acb9853ea1171fc79f3c
Khizar Hayat <khizar.hayat7eg@gmail.com>
2019-06-01 20:41:54 +0500

    Unversity Scenario Added

---
+++ README.md
@@ -0,0 +1,7 @@
+#Chatbot in Python (using NLTK library)
+
+Developers:
+Software Engineers of Bahria University
+-Khizar Hayat
+-Rafi-ul-Haq
+-Rida Fatima
---
+++ chatbot.py
@@ -0,0 +1,117 @@
+
+# coding: utf-8
+
+# # Meet Robo: your friend
+
+import io
+import random
+import string # to process standard python strings
+import warnings
+import datetime
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+warnings.filterwarnings("ignore")
+
+nltk.download('popular', quiet=True) # for downloading packages
+# Includes the following already.
+#nltk.download('punkt') # first-time use only
+#nltk.download('wordnet') # first-time use only
+
+with open('chatbot.txt','r', encoding='utf8', errors ='ignore') as fin:
+    raw = fin.read().lower()
+
+sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
+word_tokens = nltk.word_tokenize(raw)# converts to list of words
+
+
+sent_tokens[:2]
+
+
+word_tokens[:5]
+
+
+lemmer = WordNetLemmatizer()
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+GREETING_INPUTS = ("hello", "hi", "Salam", "hey", "Assalam-O-Alikum")
+GREETING_RESPONSES = ["Hello! Type Help for further options", "Type Help for all the available options",  "Type Help for Bahadur's assistance", "Type Help for Bahadur's Help"]
+
+
+
+# Checking for greetings
+def greeting(sentence):
+    """If user's input is a greeting, return a greeting response"""
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+
+
+
+# Generating response
+def response(user_response):
+    robo_response=''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        robo_response=robo_response+"I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
+
+
+flag=True
+print("##### BAHRIA UNIVERSITY HELP DESK #####")
+print("BAHADUR: Hello! My name is Bahadur. I will answer your queries regarding Bahria University Karachi Campus. If you want to exit, type Exit.")
+while(flag==True):
+    user_response =input('>>')
+    user_response=user_response.lower()
+    if(user_response!='exit'):
+        if(user_response=='thanks' or user_response=='thank you'):
+            flag=False
+            print("BAHADUR: Your welcome. Exiting..")
+
+        elif(user_response=='help'):
+            print("BAHADUR: What do you wan to ask about?")
+            print("Bahria - BUKC - Address - Gpa - Admission - Discipline - Blocks - Masters - Date - Time - Transcript - TRG - Sickbay - Cafe - Library - Scholarship - Website - Helpline ")
+        
+        elif(user_response=='date'):
+            x = datetime.datetime.now()
+            print(x.strftime("%d/%b/%Y"))
+            
+        elif(user_response=='time'):
+            x = datetime.datetime.now()
+            print(x.strftime("%I:%M:%S %p"))
+
+        else:
+            if(greeting(user_response)!=None):
+                print("BAHADUR: "+greeting(user_response))
+            else:
+                print("BAHADUR: ",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("BAHADUR: Exiting..")    
+        
+        
+
